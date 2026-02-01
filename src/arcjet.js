d025a00ea30e7c5c7cb4004ac2e991f75c28e685
sanbro <sanbro18@gmail.com>
2026-02-02 00:41:50 +0400

    applied fixed suggested by coderabbit

--- src/arcjet.js
+++ src/arcjet.js
@@ -2,34 +2,29 @@ import arcjet, {shield, detectBot, slidingWindow} from "@arcjet/node";
 
 const arcjetKey =  process.env.ARCJET_KEY;
 const arcjetMode = process.env.ARCJET_MODE === 'DRY_RUN' ? 'DRY_RUN' : 'LIVE';
-
-if(!arcjetKey) throw new Error(
-    'ARCJET_KEY environment variable is required'
-)
-
-export const httpArcjet =  arcjetKey ?
-    arcjet({
+if (!arcjetKey) {
+    throw new Error("ARCJET_KEY environment variable is required");
+}
+export const httpArcjet = arcjet({
         key: arcjetKey,
         rules: [
-            shield({ mode: arcjetMode}),
-            detectBot({ mode: arcjetMode, allow: ['CATEGORY:SEARCH_ENGINE', 'CATEGORY:PREVIEW']}),
-            slidingWindow({ mode: arcjetMode, interval: '10s', max: 50})
+            shield({ mode: arcjetMode }),
+            detectBot({ mode: arcjetMode, allow: ["CATEGORY:SEARCH_ENGINE", "CATEGORY:PREVIEW"] }),
+            slidingWindow({ mode: arcjetMode, interval: "10s", max: 50 })
         ]
-    }) : null;
+}) ;
 
-export const wsArcjet =  arcjetKey ?
-    arcjet({
+export const wsArcjet = arcjet({
         key: arcjetKey,
         rules: [
-            shield({ mode: arcjetMode}),
-            detectBot({ mode: arcjetMode, allow: ['CATEGORY:SEARCH_ENGINE', 'CATEGORY:PREVIEW']}),
-            slidingWindow({ mode: arcjetMode, interval: '2s', max: 5})
+            shield({ mode: arcjetMode }),
+            detectBot({ mode: arcjetMode, allow: ["CATEGORY:SEARCH_ENGINE", "CATEGORY:PREVIEW"] }),
+            slidingWindow({ mode: arcjetMode, interval: "2s", max: 5 })
         ]
-    }) : null;
+}) ;
 
 export function securityMiddleware(req, res, next) {
     return async (req, res, next) => {
-        if (!httpArcjet) return next();
         try{
             const decision = await httpArcjet.protect(req);
             if(decision.isDenied()){
@@ -38,11 +33,10 @@ export function securityMiddleware(req, res, next) {
                 }
                 return res.status(403).json({error: 'Forbidden'})
             }
-            
+            next();
         }catch (e) {
             console.error('Arcjet middleware failed', e);
             return res.status(503).json({error: 'Service unavailable'})
         }
-        next();
     }
 }
